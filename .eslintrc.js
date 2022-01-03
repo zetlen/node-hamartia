1badf865faf5b2ba8a24a24dc386efe0834bc0bc
James Zetlen <zetlen@gmail.com>
2022-01-03 14:18:31 -0600

    add ts, lib updates

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+export default {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+				"prettier"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+};
